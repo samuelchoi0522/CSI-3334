51c593a0922dc9b5ac2fac78d10a0f5813271c0a
kirbydyson <117674986+kirbydyson@users.noreply.github.com>
2024-04-18 20:51:55 -0500

    Update graph-proj6.cpp

--- project6/graph-proj6.cpp
+++ project6/graph-proj6.cpp
@@ -6,10 +6,30 @@
 #include <list>
 #include "arrayheap-student-proj6.h"
 
+/* addEdge
+ *  parameters:
+ *      from -- given start for edge
+ *      to -- given end for edge
+ *      cost -- given cost of going to each vertex
+ *  return value:
+ *      none
+ *
+ *  This function adds an edge connecting (from -> to) with the given cost.
+ */
 void Graph::addEdge(int from, int to, int cost){
     adjacencyList[from].push_back(Edge(to, cost));
 }
 
+/* dijkstra
+ *  parameters:
+ *      source -- given source vertex
+ *  return value:
+ *      a vector that lists the shortest path cost of going from the
+ *      source vertex to each vertex in the graph
+ *
+ *  This function finds the shortest paths to all other vertices in the
+ *  graph.
+ */
 vector<int> Graph::dijkstra(int source) const{
     vector<int> dist(adjacencyList.size(), INFINITE_COST);
     dist[source] = 0;
@@ -22,8 +42,8 @@ vector<int> Graph::dijkstra(int source) const{
         int u = pq.getMinItem().second;
         pq.removeMinItem();
 
-        for(list<Edge>::const_iterator i = adjacencyList[u].begin(); 
-        i != adjacencyList[u].end(); i++){
+        for(list<Edge>::const_iterator i = adjacencyList[u].begin();
+            i != adjacencyList[u].end(); i++){
             int v = i->to;
             int weight = i->cost;
 
